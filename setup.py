09eab338e5a06c685952ac329d4066c894c963e8
hesom <5159852+hesom@users.noreply.github.com>
2022-06-05 19:45:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         os.remove('MANIFEST')
 
     setup(name="glumpy",
-          version="1.1.1",
+          version="1.2.1",
           description="Fast, scalable & beautiful scientific visualisation",
           long_description = """Glumpy is a python library for scientific
                                 visualization that is both fast, scalable
